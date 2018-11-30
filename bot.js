cdbb502122936e0e2412ebc892736d82f2893cd6
TrariZakariaG <45496542+TrariZakariaG@users.noreply.github.com>
2018-11-30 19:58:43 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,249 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+client.login('NTE3NjMyNjMxMzQ1NTEyNDUw.DuFCiA.3xyMQlf0S2qP3oEYoeZe5rQMDMY');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var prefix = "!"
+    client.on('message', message => {
+      if (message.author.x5bz) return;
+      if (!message.content.startsWith(prefix)) return;
+     
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+     
+      let args = message.content.split(" ").slice(1);
+     
+      if (command == "ban") {
+                   if(!message.channel.guild) return message.reply('** This command only for servers**');
+             
+      if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+      if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+      let user = message.mentions.users.first();
+      let reason = message.content.split(" ").slice(2).join(" ");
+      /*let b5bzlog = client.channels.find("name", "5bz-log");
+     
+      if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+      if (message.mentions.users.size < 1) return message.reply("**???? ???**");
+      if(!reason) return message.reply ("**???? ??? ?????**");
+      if (!message.guild.member(user)
+      .bannable) return message.reply("**???????? ??? ??? ???? ?? ????? ???? ????? ????? ???? ????**");
+     
+      message.guild.member(user).ban(7, user);
+     
+      const banembed = new Discord.RichEmbed()
+      .setAuthor(`BANNED!`, user.displayAvatarURL)
+      .setColor("RANDOM")
+      .setTimestamp()
+      .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+      .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+      .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+      message.channel.send({
+        embed : banembed
+      })
+    }
+    });
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 client.on('message', message => {
+    const prefix = "!";
+      if (message.author.kick) return;
+      if (!message.content.startsWith(prefix)) return;
+     
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+     
+      let args = message.content.split(" ").slice(1);
+     
+      if (command == "kick") {
+                   if(!message.channel.guild) return;
+             
+      if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
+      if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
+      let user = message.mentions.users.first();
+      let reason = message.content.split(" ").slice(2).join(" ");
+     
+      if (message.mentions.users.size < 1) return message.reply("???? ???");
+      if(!reason) return message.reply ("???? ??? ?????");
+      if (!message.guild.member(user)
+      .bannable) return message.reply("???????? ??? ??? ???? ?? ?????");
+     
+      message.guild.member(user).kick(7, user);
+     
+      const banembed = new Discord.RichEmbed()
+      .setAuthor('Kicked !', user.displayAvatarURL)
+      .setColor("RANDOM")
+      .setTimestamp()
+      .addField("User:",  `[ + ${user.tag} + ]`)
+      .addField("By:", `[  + ${message.author.tag} +  ]`)
+      .addField("Reason:", `[ + ${reason} +  ]`)
+      client.channels.get("492583022982463500").send({embed : banembed})
+    }
+    });
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	let ar = JSON.parse(fs.readFileSync(`AutoRole.json`, `utf8`))
+client.on('message', message => {
+  var sender = message.author
+ 
+if(!message.guild) return
+  if(!ar[message.guild.id]) ar[message.guild.id] = {
+  onoff: 'Off',
+  role: 'Member'
+  }
+ 
+if(message.content.startsWith(`!autorole`)) {
+         
+  let perms = message.member.hasPermission(`MANAGE_ROLES`)
+ 
+  if(!perms) return message.reply(`You don't have permissions, required permission : Manage Roles.`)
+ let args = message.content.split(" ").slice(1)
+ if(!args.join(" ")) return message.reply(`${prefix}autorole toggle / set [ROLE NAME]`)
+ let state = args[0]
+ if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'setrole') return message.reply(`Please type a right state, ${prefix}modlogs toggle/setrole [ROLE NAME]`)
+   if(state.trim().toLowerCase() == 'toggle') {
+    if(ar[message.guild.id].onoff === 'Off') return [message.channel.send(`**The Autorole Is __????__ !**`), ar[message.guild.id].onoff = 'On']
+    if(ar[message.guild.id].onoff === 'On') return [message.channel.send(`**The Autorole Is __??????__ !**`), ar[message.guild.id].onoff = 'Off']
+   }
+  if(state.trim().toLowerCase() == 'set') {
+  let newRole = message.content.split(" ").slice(2).join(" ")
+  if(!newRole) return message.reply(`${prefix}autorole set [ROLE NAME]`)
+    if(!message.guild.roles.find(`name`,newRole)) return message.reply(`I Cant Find This Role.`)
+   ar[message.guild.id].role = newRole
+    message.channel.send(`**The AutoRole Has Been Changed to ${newRole}.**`)
+  }
+        }
+if(message.content === '!info') {
+   let perms = message.member.hasPermission(`MANAGE_GUILD`)
+   if(!perms) return message.reply(`You don't have permissions.`)
+    var embed = new Discord.RichEmbed()
+ 
+.addField(`Autorole : :sparkles:  `, `
+State : __${ar[message.guild.id].onoff}__
+Role : __${ar[message.guild.id].role}__`)
+ 
+ 
+    .setColor(`BLUE`)
+    message.channel.send({embed})
+  }
+ 
+ 
+    fs.writeFile("./AutoRole.json", JSON.stringify(ar), (err) => {
+    if (err) console.error(err)
+  });
+ 
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
